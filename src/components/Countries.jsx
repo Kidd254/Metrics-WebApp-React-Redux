ea68cb37a539ca45233d0e80f73d98d1adbe23db
Kidd254 <lorenkioko@gmail.com>
2023-07-30 23:52:35 +0300

    Add components to display main page

---
+++ src/components/Countries.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+
+const Countries = () =>{
+    return(
+        <div>
+        <Navbar />
+        </div>
+    )
+}
+
+export default Countries;
---
+++ src/components/Country.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar";
+
+const Country = () =>{
+    return(
+        <div>
+        <Navbar />
+       </div>
+    )
+}
+ export default Country;
