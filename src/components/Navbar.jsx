6c528f396aa72c29d40860dc39ded89c48c170b5
Kidd254 <lorenkioko@gmail.com>
2023-07-30 23:52:00 +0300

    Set up the nav bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { NavLink } from 'react-router-dom';
+import { IoChevronBack } from 'react-icons/io5';
+import { AiFillSetting, AiFillAudio } from 'react-icons/ai';
+import NavbarCss from '../styles/Navbar.module.css';
+const Navbar = () => (
+  <div className={NavbarCss.navContainer} data-testid="navContainer">
+    <NavLink to="/" className={NavbarCss.navLink}>
+      <IoChevronBack />
+    </NavLink>
+    <div className={NavbarCss.navLinkIcons}>
+      <AiFillAudio />
+      <AiFillSetting />
+    </div>
+  </div>
+);
+
+export default Navbar;
