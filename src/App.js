2ae89bc20d5b12ab37360be4b4c36978dccdcf86
Kidd254 <lorenkioko@gmail.com>
2023-07-30 23:54:17 +0300

    Set up routes to render

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import { Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import './App.css';
+import Countries from './components/Countries';
+import Country from './components/Country';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+   <Routes>
+    <Route path='/' element={<Countries />}/>
+    <Route path='/:id' element={<Country />} />
+   </Routes>
+   </div>
   );
 }
 
