200298aa274ea8886bc195e174267c952987a927
Kidd254 <lorenkioko@gmail.com>
2023-07-30 23:55:06 +0300

    Add Provider and set up to display content

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+       <App />
+    </BrowserRouter>
+ 
+  </Provider>
+    
 );
 
 // If you want to start measuring performance in your app, pass a function
