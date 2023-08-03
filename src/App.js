719d1dfd2f0141ecfef26e9f9a38bcff7a23352c
Kidd254 <lorenkioko@gmail.com>
2023-08-03 19:43:53 +0300

    Dispatch stored data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import Countries from './components/Countries';
 import Country from './components/Country';
+import { getDataFromApi } from './redux/country/countrySlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getDataFromApi());
+  }, [dispatch]);
   return (
     <div>
       <Routes>
