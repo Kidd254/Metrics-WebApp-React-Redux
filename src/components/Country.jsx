eb5ba30789d09b5a9a1392900463a4bc0035737a
Kidd254 <lorenkioko@gmail.com>
2023-08-05 16:50:45 +0300

    Match my style to the requested change

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -12,50 +12,41 @@ const Country = () => {
     <div className={styles.countryMainContainer} data-testid="details">
       <div className={styles.countryContainer}>
         <Navbar />
-        { country && (
-        <div>
+        {country && (
           <div>
             <div className={styles.countryAndFlagItem}>
-              {' '}
               <span>
-                {' '}
                 <img src={country.flagImage} className={styles.ctryFlag} alt={country.flag} />
-                {' '}
               </span>
-              {' '}
               <span className={styles.countryFlagName}>{country.flag}</span>
-              {' '}
             </div>
+            <ul className={styles.countryInformation}>
+              <li className={styles.countryInformationList}>
+                <span className={styles.key}>Capital City:</span>
+                <span className={styles.value}>{country.capital}</span>
+              </li>
+              <li className={styles.countryInformationList}>
+                <span className={styles.key}>Population:</span>
+                <span className={styles.value}>{country.population}</span>
+              </li>
+              <li className={styles.countryInformationList}>
+                <span className={styles.key}>Continent:</span>
+                <span className={styles.value}>{country.continent}</span>
+              </li>
+              <li className={styles.countryInformationList}>
+                <span className={styles.key}>Region:</span>
+                <span className={styles.value}>{country.region}</span>
+              </li>
+              <li className={styles.countryInformationList}>
+                <span className={styles.key}>Start of Week:</span>
+                <span className={styles.value}>{country.week}</span>
+              </li>
+            </ul>
           </div>
-          <ul className={styles.countryInformation}>
-            <li className={styles.countryInformationList}>
-              Capital City:
-              {country.capital}
-            </li>
-            <li className={styles.countryInformationList}>
-              Population:
-              {country.population}
-            </li>
-            <li className={styles.countryInformationList}>
-              {' '}
-              Continent:
-              {country.continent}
-            </li>
-            <li className={styles.countryInformationList}>
-              {' '}
-              Region:
-              {country.region}
-            </li>
-            <li className={styles.countryInformationList}>
-              {' '}
-              Start of Week:
-              {country.startOfWeek}
-            </li>
-          </ul>
-        </div>
         )}
       </div>
     </div>
   );
 };
+
 export default Country;
